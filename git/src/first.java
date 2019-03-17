1bfc5e0f885e374837decad99ea425d4df6dbba8
zhangsan <zhangsan@it.cast>
2019-03-17 19:43:08 +0800

    提交

--- git/src/first.java
+++ git/src/first.java
@@ -2,4 +2,9 @@ public class first {
     public static void main(String[] args) {
         System.out.println("ssss");
     }
+    public void yyy(){
+        int i =10;
+        int b =20;
+        System.out.println(i+b);
+    }
 }
